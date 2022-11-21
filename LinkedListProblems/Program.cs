8e4ca25f99f28018fa9961e259a936418beb8a9f
lalitraghu1c <raghuwanshilalit5@gmail.com>
2022-11-21 12:14:37 +0530

    [Lalit] Add: Folder Structure Created

---
+++ LinkedListProblems/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedListProblems
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linkedlist problem Statement");
+        }
+    }
+}
